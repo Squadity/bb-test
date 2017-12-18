9b4e72e3d00792e5bb98ca8c8439a5423f8fea0a
Alexandr Bolbat <alexandr@bolbat.net>
2017-12-18 18:37:40 +0200

    fixed javadoc artifact generation

---
+++ src/main/java/net/bolbat/test/MockJavaDoc.java
@@ -0,0 +1,10 @@
+package net.bolbat.test;
+
+/**
+ * This class is required for 'maven-javadoc-plugin' for generating and deploying 'Javadoc' artifact.
+ * 
+ * @author Alexandr Bolbat
+ */
+public class MockJavaDoc {
+
+}
